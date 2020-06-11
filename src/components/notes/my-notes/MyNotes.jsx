1dc052b1f496df1ab447f29a9712b9768c087435
RossRekin <markixbg@gmail.com>
2020-06-12 01:54:47 +0300

    task delete fix + some css fixes

--- src/components/notes/my-notes/MyNotes.jsx
+++ src/components/notes/my-notes/MyNotes.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { NoteCard } from '../note-card/NoteCard';
-import { getNoteByAuthorId, getMyNotes } from '../../../core/api/notes.api';
+import { getNoteByAuthorId, getMyNotes, deleteNote } from '../../../core/api/notes.api';
+
+const listStyles = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: '10ox'
+};
+const cardStyle={
+    flexWrap:'wrap'
+}
 
 export function MyNotes(props) {
 
@@ -12,10 +21,18 @@ export function MyNotes(props) {
             setUserNotes(notes);
         });
     }, []);
+	
+	const onDelete = (id) => {
+        deleteNote(id).then(() => {
+            setUserNotes((prevState) => {
+                return prevState.filter(note => note.id !== id);
+            })
+        })
+    };
 
     return (
-        <div className="my-notes-wrapper">
-            {userNotes.map(note => <NoteCard note={note} key={note.id} />)}
+         <div className="notes-list d-flex" style={cardStyle}>
+            {userNotes.map(note => <NoteCard note={note} key={note.id} onDeleteClick={onDelete} />)}
         </div>
     )
 }
